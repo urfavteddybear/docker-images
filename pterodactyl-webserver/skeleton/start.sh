871f246734012300e13ea531acb688cdc16e336f
urfavteddybear <business.urfavteddybear@gmail.com>
2026-02-11 02:48:10 +0800

    fix: update Node.js version to 24 LTS and clean up start.sh script

--- pterodactyl-webserver/skeleton/start.sh
+++ pterodactyl-webserver/skeleton/start.sh
@@ -3,9 +3,6 @@ set -e
 
 cd /home/container
 
-mkdir -p tmp logs
-chmod -R 755 tmp logs webroot || true
-rm -rf tmp/* || true
 
 echo "Starting PHP-FPM..."
 php-fpm8.2 --fpm-config /home/container/php-fpm/php-fpm.conf
