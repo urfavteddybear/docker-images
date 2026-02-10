30ca407424b2646888db3b236a0756446bb6fab5
urfavteddybear <business.urfavteddybear@gmail.com>
2026-02-11 02:51:55 +0800

    fix: streamline entrypoint and start scripts for improved server initialization

--- pterodactyl-webserver/entrypoint.sh
+++ pterodactyl-webserver/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-set -e
+sleep 2
 
 cd /home/container
 
-# Init server files on first run
-if [ -z "$(ls -A /home/container)" ]; then
-    echo "Initializing server directory..."
-    cp -r /skeleton/* /home/container/
-    chmod +x /home/container/start.sh
-fi
-
-MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP_CMD}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-exec ${MODIFIED_STARTUP}
+# Run the Server
+${MODIFIED_STARTUP}
--- pterodactyl-webserver/skeleton/start.sh
+++ pterodactyl-webserver/skeleton/start.sh
@@ -5,7 +5,7 @@ cd /home/container
 
 
 echo "Starting PHP-FPM..."
-php-fpm8.2 --fpm-config /home/container/php-fpm/php-fpm.conf
+php-fpm8.2 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-exec nginx -c /home/container/nginx/nginx.conf -p /home/container/ -g "daemon off;"
+exec nginx -c /home/container/nginx/nginx.conf -p /home/container/
