09a4decd8bf802901d31877f5e29806f0f168678
urfavteddybear <business.urfavteddybear@gmail.com>
2026-02-11 13:12:24 +0800

    chore: removing cron

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-echo "Starting CRON..."
-crontab /home/container/crontab
-crond -L /home/container/logs/cron.log
-
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm84 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
