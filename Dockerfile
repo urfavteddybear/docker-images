8f00fa91666e326b68bcb098c8f2e5857105cbb3
urfavteddybear <business.urfavteddybear@gmail.com>
2026-02-11 12:35:14 +0800

    feat: added cron

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN apk update --no-cache && apk add \
     bash \
     nodejs \
     npm \
+    dcron \
     imagemagick \
     php \
     php-fpm \
--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
+echo "Starting CRON..."
+crontab /home/container/crontab
+crond -L /home/container/logs/cron.log
+
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm84 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
